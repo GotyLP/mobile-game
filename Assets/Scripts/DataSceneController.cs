90021ff661dd7ea8f848975f3a8a97261648de34
LukasMDQ <123424547+LukasMDQ@users.noreply.github.com>
2025-06-24 23:58:27 -0300

    data

---
+++ Assets/Scripts/DataSceneController.cs
@@ -0,0 +1,27 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataSceneController : MonoBehaviour
+{
+   
+    public static DataSceneController Instance;
+    public LifeHandler lifeHandler;
+    [SerializeField] private float life;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if (Instance != this)
+            {
+                Destroy(gameObject);
+            }            
+        }
+    }
+}
